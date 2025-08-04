e8bf445ab9a819b2067f47fe2dc27319a9371379
Pedro1AF <pedro.augusto.f.14@gmail.com>
2025-08-04 10:22:45 -0300

    python atualizado

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, request, url_for, redirect, flash, ses
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from db import db
-from models import User
+from models import User, paciente
 
 app = Flask(__name__)
 app.secret_key = 'dev'
 lm = LoginManager(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data_sistem.db"
 db.init_app(app)
 
 
@@ -16,20 +16,20 @@ def load_user(id):
     usuario_logado = db.session.query(User).filter_by(id=id).first()
     return usuario_logado
 
-#@app.route('/criar_conta', methods=['GET', 'POST'])
-#def criar_conta():
-#    if request.method == 'GET':
-#        return render_template('criar.html')
-#    
-#    elif request.method == 'POST':
-#        nome = request.form['nome']
-#        crm = request.form['crm']
-#        senha = request.form['senha']
-#        novo_user = User(nome=nome, crm=crm, password=senha)
-#        db.session.add(novo_user)
-#        db.session.commit()
-#        login_user(novo_user)
-#    return redirect(url_for('login'))
+@app.route('/criar_conta', methods=['GET', 'POST'])
+def criar_conta():
+    if request.method == 'GET':
+        return render_template('criar.html')
+    
+    elif request.method == 'POST':
+        nome = request.form['nome']
+        crm = request.form['crm']
+        senha = request.form['senha']
+        novo_user = User(nome=nome, crm=crm, password=senha)
+        db.session.add(novo_user)
+        db.session.commit()
+        login_user(novo_user)
+    return redirect(url_for('login'))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -44,6 +44,8 @@ def login():
         
         if usuario and usuario.password == senha:
             login_user(usuario)
+            if current_user.nome == 'dev.admin':
+                return redirect(url_for('admin'))
             return redirect(url_for('index'))
         else:
             flash('crm ou senha incorretos.')
@@ -58,7 +60,9 @@ def admin():
             
     elif request.method == 'GET': 
         usuarios_lista = db.session.query(User).all()
-        return render_template('adm.html', usuario_consulta=usuarios_lista)
+        pacientes_lista = db.session.query(paciente).all()
+        return render_template("adm.html", usuarios_banco=usuarios_lista, pacientes_banco =pacientes_lista)
+        
     
 # (adicionar as condiçoes para as requisições GET e POST) 
     elif request.method == 'POST':
@@ -69,14 +73,41 @@ def admin():
          db.session.add(novo_user)
          db.session.commit()
     return redirect(url_for('admin'))
-       
-        
+     
+@app.route('/delete_medicos/<int:id>')
+def delete_medicos(id):
+    usuario = db.session.query(User).filter_by(id=id).first()
+    db.session.delete(usuario)
+    db.session.commit()
+    return redirect(url_for('admin'))
 
-@app.route('/pagina_principal')
+@app.route('/delete_pacientes/<int:id>')
+def delete_paciente(id):
+    pacientes = db.session.query(paciente).filter_by(id=id).first()
+    db.session.delete(pacientes)
+    db.session.commit()
+    return redirect(url_for('admin'))
+    
+
+@app.route('/pagina_principal', methods=['GET', 'POST'])
 @login_required
 def index():
-    load_user(current_user.nome)
-    return render_template('index.html')
+    
+    if request.method == 'GET':
+        load_user(current_user.nome)
+        pacientes_lista = db.session.query(paciente).all()
+        return render_template('index.html', pacientes_consulta = pacientes_lista)
+    
+    elif request.method == 'POST':
+        nome_paciente = request.form['nome']
+        idade_paciente = request.form['idade']
+        sexo_paciente = request.form['sexo']
+        descricao_paciente = request.form['descricao']
+        novo_paciente = paciente(nome = nome_paciente, idade = idade_paciente, sexo = sexo_paciente, descricao = descricao_paciente)
+        db.session.add(novo_paciente)
+        db.session.commit()
+        login_user(novo_paciente)
+    return redirect(url_for('index'))
 
 
 
--- models.py
+++ models.py
@@ -7,3 +7,11 @@ class User(UserMixin, db.Model):
     crm = db.Column(db.String(10), unique=True, nullable=False)
     nome = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    
+class paciente(UserMixin, db.Model):
+    __tablename__ = 'pacientes'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), unique=True, nullable=False)
+    sexo = db.Column(db.String(50), nullable=False)
+    idade = db.Column(db.String(3), nullable=False)
+    descricao = db.Column(db.String(250), nullable=False)
