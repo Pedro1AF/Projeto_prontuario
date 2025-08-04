22d2b9cbd277abeb3080ef083936666bcfdbf657
Pedro1AF <pedro.augusto.f.14@gmail.com>
2025-08-04 10:25:39 -0300

    css e js atualizados e linkados

--- static/main.js
+++ static/main.js
@@ -1,7 +1,28 @@
-function alert() {
-    Swal.fire({
-        title: "Good job!",
-        text: "You clicked the button!",
-        icon: "success"
+
+
+    function alert() {
+        Swal.fire({
+            title: "Good job!",
+            text: "You clicked the button!",
+            icon: "success"
+        });
+    }
+
+
+    function checkboxExclusivo(clicked) {
+        const checkboxes = document.getElementsByName("sexo");
+        checkboxes.forEach((box) => {
+            if (box !== clicked) {
+                box.checked = false;
+            }
+        });
+    }
+
+    const elemSelect = document.querySelector("#selctModo");
+    elemSelect.addEventListener("change", function () {
+        if (elemSelect.value == "light") {
+            document.documentElement.setAttribute("modo-light-dark", "light");
+        } else if (elemSelect.value == "dark") {
+            document.documentElement.setAttribute("modo-light-dark", "dark");
+        }
     });
-}
